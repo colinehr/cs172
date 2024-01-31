748b521d7a2d556353ba5827194731693dab3ba1
Colin Ehr <colinehr@lclark.edu>
2024-01-31 11:25:54 -0800

    in-class work for 01-31

--- code/all_digits_odd.py
+++ code/all_digits_odd.py
@@ -1,18 +1,22 @@
-# User inputs a number
-# Tells us if every digit is odd or if at least one is even
-num = input("Give me a whole number: ")
-i = 0
-even = False
-zero = False
-while i < len(num):
-    if int(num[i]) % 2 == 0:
-        even = True
-    if int(num[i]) == 0:
-        zero = True
-    i += 1
-if zero:
-    print("There was a zero.")
-elif not even:
-    print("All digits are odd!")
-else:
-    print("Not all digits are odd. :(")
+def all_digits_odd(num: str) -> bool:
+    # User inputs a number
+    # Tells us if every digit is odd or if at least one is even
+    i = 0
+    even = False
+    while i < len(num):
+        if int(num[i]) % 2 == 0:
+            even = True
+        i += 1
+    if not even:
+        return True
+    else:
+        return False
+
+
+n = input("Give me a whole number: ")
+while n != "":
+    if all_digits_odd(n):
+        print("All digits are odd!")
+    else:
+        print("At least one digit is even. :(")
+    n = input("Give me a whole number: ")
