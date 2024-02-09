3feda47f9f26226eb75c34d34f05e81ffa8fa67f
Colin Ehr <colinehr@lclark.edu>
2024-02-09 11:26:45 -0800

    in-class code 02-09

--- code/dice.py
+++ code/dice.py
@@ -7,15 +7,24 @@ import random
 
 def roll(sides: int, num_dice: int) -> int:
     # Return an integer that is the sum of the dice thrown
-    pass
+    rolls = []
+    i = 0
+    while i < num_dice:
+        rolls.append(random.randint(1, sides))
+        i += 1
+    return sum(rolls)
 
 
-counts = [0] * 13
+number_of_dice = 3
+
+counts = [0] * (number_of_dice * 6 + 1)
 i = 0
 while i < 10000:
-    roll = random.randint(1, 6) + random.randint(1, 6)
-    counts[roll] += 1
+    throw = roll(6, number_of_dice)
+    counts[throw] += 1
     i += 1
-print(counts)
 
-# Modify this to give a more human-friendly output (bonus: give percents)
+i = number_of_dice
+while i < len(counts):
+    print(str(i) + ": " + str(counts[i]))
+    i += 1
